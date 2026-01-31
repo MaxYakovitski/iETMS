fc7b3a527f08c8d554df002a0cf52fa19e090cc1
max-yakovitski <m.yakovitski@gmail.com>
2026-01-31 16:18:38 +0300

    fix(statistics): fix r_dispatched alias in user statistics query

--- iet-backend/src/main/java/com/mayak/iet/features/statistics/infra/persistence/UserStatisticsRepository.java
+++ iet-backend/src/main/java/com/mayak/iet/features/statistics/infra/persistence/UserStatisticsRepository.java
@@ -75,8 +75,8 @@ public interface UserStatisticsRepository extends Repository <User, Long> {
         LEFT JOIN requests r
             ON r.id = fb.request_id
 
-        LEFT JOIN requests dispatched
-            ON r_dispatched.dispatched_user_id = u.id
+        LEFT JOIN requests r_dispatched
+            ON r_dispatched.dispatcher_id = u.id
            AND r_dispatched.issue_date BETWEEN :from AND :to
 
         WHERE u.id = ANY(:userIds)
