6f8d38358390d1dd49b9809b1560589ff8230063
max-yakovitski <m.yakovitski@gmail.com>
2026-01-31 14:46:03 +0300

    Remove native messaging host and simplify desktop architecture

--- iet-desktop/src/main/resources/installer/register-native-host.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-reg add ^
- "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.mayak.iet.nativehost" ^
- /ve /t REG_SZ ^
- /d "C:\Program Files\iETMS\app\native-host\com.mayak.iet.nativehost.json" ^
- /f
