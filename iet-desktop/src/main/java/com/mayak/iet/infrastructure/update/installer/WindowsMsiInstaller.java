4b113da2444ef0de317d7ef3a198d06987e5accc
max-yakovitski <m.yakovitski@gmail.com>
2026-02-11 12:18:04 +0300

    added logs

--- iet-desktop/src/main/java/com/mayak/iet/infrastructure/update/installer/WindowsMsiInstaller.java
+++ iet-desktop/src/main/java/com/mayak/iet/infrastructure/update/installer/WindowsMsiInstaller.java
@@ -4,6 +4,8 @@ import com.mayak.iet.infrastructure.update.UpdatePaths;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 @Service
@@ -20,12 +22,36 @@ public class WindowsMsiInstaller implements UpdateInstaller {
 
         Path updaterJar = UpdatePaths.updaterJar();
 
-        new ProcessBuilder(
-                javaExe.toString(),
-                "-jar",
-                updaterJar.toString(),
-                msiFile.toString()
-        ).start();
+        Path logFile = UpdatePaths.updatesDir().resolve("installer-launch.log");
+        Files.createDirectories(logFile.getParent());
+
+        try {
+            Files.writeString(logFile,
+                    "JAVA: " + javaExe + "\n" +
+                            "UPDATER: " + updaterJar + "\n" +
+                            "MSI: " + msiFile + "\n",
+                    java.nio.file.StandardOpenOption.CREATE,
+                    java.nio.file.StandardOpenOption.TRUNCATE_EXISTING
+            );
+
+            ProcessBuilder pb = new ProcessBuilder(
+                    javaExe.toString(),
+                    "-jar",
+                    updaterJar.toString(),
+                    msiFile.toString()
+            );
+
+            pb.redirectOutput(logFile.toFile());
+            pb.redirectErrorStream(true);
+
+            pb.start();
+
+        } catch (IOException e) {
+            Files.writeString(logFile,
+                    "\nERROR: " + e.getMessage(),
+                    java.nio.file.StandardOpenOption.APPEND);
+            throw e;
+        }
 
         System.exit(0);
     }
