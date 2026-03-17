0a9bf5a33da7a740ad0f74ac363a5409d669a392
max-yakovitski <m.yakovitski@gmail.com>
2026-03-18 00:20:58 +0300

    DraftValidationErrorResponse with non null

--- ietms-contract/src/main/java/com/mayak/ietms/extension/dto/DraftValidationErrorResponse.java
+++ ietms-contract/src/main/java/com/mayak/ietms/extension/dto/DraftValidationErrorResponse.java
@@ -1,8 +1,11 @@
 package com.mayak.ietms.extension.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 import java.util.Map;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record DraftValidationErrorResponse (
         ExtensionDraftIntent intent,
         Map<String, List<String>> errors) implements ExtensionDraftResponse {
